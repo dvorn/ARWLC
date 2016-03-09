0c8b123b99415fb0b41988e390df8e129c8281e5
dvorn <sgd@list.ru>
2016-03-09 12:51:34 +0600

    Initial implementation
    Async reader-writer lock with cancellation initial implementation

---
+++ ARWLC.Tests/AsyncReaderWriterLockUnitTests.cs
@@ -0,0 +1,221 @@
+﻿namespace ARWLC.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using System.Linq;
+    using System.Threading;
+    using System.Diagnostics.CodeAnalysis;
+
+    using ARWLC;
+
+    [ExcludeFromCodeCoverage]
+    [TestClass]
+    public class AsyncReaderWriterLockUnitTests
+    {
+        [TestMethod]
+        public async Task Unlocked_PermitsWriterLock()
+        {
+            var rwl = new AsyncReaderWriterLock();
+            await rwl.WriterLockAsync();
+            var task = rwl.WriterLockAsync();
+            await AssertEx.NeverCompletesAsync(task);
+        }
+
+        [TestMethod]
+        public async Task Unlocked_PermitsMultipleReaderLocks()
+        {
+            var rwl = new AsyncReaderWriterLock();
+            await rwl.ReaderLockAsync();
+            await rwl.ReaderLockAsync();
+        }
+
+        [TestMethod]
+        public async Task WriteLocked_PreventsAnotherWriterLock()
+        {
+            var rwl = new AsyncReaderWriterLock();
+            await rwl.WriterLockAsync();
+            var task = rwl.WriterLockAsync();
+            await AssertEx.NeverCompletesAsync(task);
+        }
+
+        [TestMethod]
+        public async Task WriteLocked_PreventsReaderLock()
+        {
+            var rwl = new AsyncReaderWriterLock();
+            await rwl.WriterLockAsync();
+            var task = rwl.ReaderLockAsync();
+            await AssertEx.NeverCompletesAsync(task);
+        }
+
+        [TestMethod]
+        public async Task WriteLocked_Unlocked_PermitsAnotherWriterLock()
+        {
+            var rwl = new AsyncReaderWriterLock();
+            var firstWriteLockTaken = new TaskCompletionSource<object>();
+            var releaseFirstWriteLock = new TaskCompletionSource<object>();
+            var task = Task.Run(async () =>
+            {
+                using (await rwl.WriterLockAsync())
+                {
+                    firstWriteLockTaken.SetResult(null);
+                    await releaseFirstWriteLock.Task;
+                }
+            });
+            await firstWriteLockTaken.Task;
+            var lockTask = rwl.WriterLockAsync();
+            Assert.IsFalse(lockTask.IsCompleted);
+            releaseFirstWriteLock.SetResult(null);
+            await lockTask;
+        }
+
+        [TestMethod]
+        public async Task ReadLocked_PreventsWriterLock()
+        {
+            var rwl = new AsyncReaderWriterLock();
+            await rwl.ReaderLockAsync();
+            var task = rwl.WriterLockAsync();
+            await AssertEx.NeverCompletesAsync(task);
+        }
+
+        [TestMethod]
+        public void WriterLock_PreCancelled_LockAvailable_SynchronouslyTakesLock()
+        {
+            var rwl = new AsyncReaderWriterLock();
+            var token = new CancellationToken(true);
+
+            var task = rwl.WriterLockAsync(token);
+
+            Assert.IsTrue(task.IsCompleted);
+            Assert.IsFalse(task.IsCanceled);
+            Assert.IsFalse(task.IsFaulted);
+        }
+
+        [TestMethod]
+        public void WriterLock_PreCancelled_LockNotAvailable_SynchronouslyCancels()
+        {
+            var rwl = new AsyncReaderWriterLock();
+            var token = new CancellationToken(true);
+            rwl.WriterLockAsync();
+
+            var task = rwl.WriterLockAsync(token);
+
+            Assert.IsTrue(task.IsCompleted);
+            Assert.IsTrue(task.IsCanceled);
+            Assert.IsFalse(task.IsFaulted);
+        }
+
+        [TestMethod]
+        public void ReaderLock_PreCancelled_LockAvailable_SynchronouslyTakesLock()
+        {
+            var rwl = new AsyncReaderWriterLock();
+            var token = new CancellationToken(true);
+
+            var task = rwl.ReaderLockAsync(token);
+
+            Assert.IsTrue(task.IsCompleted);
+            Assert.IsFalse(task.IsCanceled);
+            Assert.IsFalse(task.IsFaulted);
+        }
+
+        [TestMethod]
+        public void ReaderLock_PreCancelled_LockNotAvailable_SynchronouslyCancels()
+        {
+            var rwl = new AsyncReaderWriterLock();
+            var token = new CancellationToken(true);
+            rwl.WriterLockAsync();
+
+            var task = rwl.ReaderLockAsync(token);
+
+            Assert.IsTrue(task.IsCompleted);
+            Assert.IsTrue(task.IsCanceled);
+            Assert.IsFalse(task.IsFaulted);
+        }
+
+        [TestMethod]
+        public async Task WriteLocked_WriterLockCancelled_DoesNotTakeLockWhenUnlocked()
+        {
+            var rwl = new AsyncReaderWriterLock();
+            using (await rwl.WriterLockAsync())
+            {
+                var cts = new CancellationTokenSource();
+                var task = rwl.WriterLockAsync(cts.Token);
+                cts.Cancel();
+                await AssertEx.ThrowsExceptionAsync<OperationCanceledException>(() => task);
+            }
+
+            await rwl.WriterLockAsync();
+        }
+
+        [TestMethod]
+        public async Task WriteLocked_ReaderLockCancelled_DoesNotTakeLockWhenUnlocked()
+        {
+            var rwl = new AsyncReaderWriterLock();
+            using (await rwl.WriterLockAsync())
+            {
+                var cts = new CancellationTokenSource();
+                var task = rwl.ReaderLockAsync(cts.Token);
+                cts.Cancel();
+                await AssertEx.ThrowsExceptionAsync<OperationCanceledException>(() => task);
+            }
+
+            await rwl.ReaderLockAsync();
+        }
+
+        [TestMethod]
+        public async Task LockReleased_WriteTakesPriorityOverRead()
+        {
+            var rwl = new AsyncReaderWriterLock();
+            Task writeLock, readLock;
+            using (await rwl.WriterLockAsync())
+            {
+                readLock = rwl.ReaderLockAsync();
+                writeLock = rwl.WriterLockAsync();
+            }
+
+            await writeLock;
+            await AssertEx.NeverCompletesAsync(readLock);
+        }
+
+        [TestMethod]
+        public async Task ReaderLocked_ReaderReleased_ReaderAndWriterWaiting_DoesNotReleaseReaderOrWriter()
+        {
+            var rwl = new AsyncReaderWriterLock();
+            Task readLock, writeLock;
+            await rwl.ReaderLockAsync();
+            using (await rwl.ReaderLockAsync())
+            {
+                writeLock = rwl.WriterLockAsync();
+                readLock = rwl.ReaderLockAsync();
+            }
+
+            await Task.WhenAll(AssertEx.NeverCompletesAsync(writeLock),
+                AssertEx.NeverCompletesAsync(readLock));
+        }
+
+#if (false)
+        [TestMethod]
+        public void LoadTest()
+        {
+            Test.Async(async () =>
+            {
+                var rwl = new AsyncReaderWriterLock();
+                var readKeys = new List<IDisposable>();
+                for (int i = 0; i != 1000; ++i)
+                    readKeys.Add(rwl.ReaderLock());
+                var writeTask = Task.Run(() => { rwl.WriterLock().Dispose(); });
+                var readTasks = new List<Task>();
+                for (int i = 0; i != 100; ++i)
+                    readTasks.Add(Task.Run(() => rwl.ReaderLock().Dispose()));
+                await Task.Delay(1000);
+                foreach (var readKey in readKeys)
+                    readKey.Dispose();
+                await writeTask;
+                foreach (var readTask in readTasks)
+                    await readTask;
+            });
+        }
+#endif
+    }
+}
---
+++ ARWLC/AsyncReaderWriterLock.cs
@@ -0,0 +1,247 @@
+﻿namespace ARWLC
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Defines a lock that supports single writers and multiple readers.
+    /// </summary>
+    public class AsyncReaderWriterLock
+    {
+        private readonly Task<Releaser> readerReleaser;
+        private readonly Task<Releaser> writerReleaser;
+
+        private readonly List<Waiter> waitingWriters = new List<Waiter>();
+        private readonly List<Waiter> waitingReaders = new List<Waiter>();
+
+        private int status; // -1 writer locked, 0 idle, n > 0 number of reader locks
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AsyncReaderWriterLock" /> class.
+        /// </summary>
+        public AsyncReaderWriterLock()
+        {
+            TaskCompletionSource<Releaser> tcs = new TaskCompletionSource<Releaser>();
+            tcs.SetResult(new Releaser(this, false));
+            this.readerReleaser = tcs.Task;
+
+            tcs = new TaskCompletionSource<Releaser>();
+            tcs.SetResult(new Releaser(this, true));
+            this.writerReleaser = tcs.Task;
+        }
+
+        public Task<Releaser> ReaderLockAsync()
+        {
+            return this.ReaderLockAsync(CancellationToken.None);
+        }
+
+        /// <summary>
+        /// ReaderLockAsync is used when a new reader wants in.
+        /// </summary>
+        /// <param name="token">The token.</param>
+        /// <returns></returns>
+        public Task<Releaser> ReaderLockAsync(CancellationToken token)
+        {
+            lock (this.waitingWriters)
+            {
+                if (this.status >= 0 && this.waitingWriters.Count == 0)
+                {
+                    ++this.status;
+                    return this.readerReleaser;
+                }
+
+                Waiter waiter = new Waiter { Tcs = new TaskCompletionSource<Releaser>() };
+                waiter.Registration = token.Register(() => this.OnReaderCancel(waiter));
+
+                this.waitingReaders.Add(waiter);
+
+                return waiter.Tcs.Task;
+            }
+        }
+
+        public Task<Releaser> WriterLockAsync()
+        {
+            return this.WriterLockAsync(CancellationToken.None);
+        }
+
+        /// <summary>
+        /// WriterLockAsync is used when a new writer wants in.
+        /// </summary>
+        /// <param name="token">The token.</param>
+        /// <returns></returns>
+        public Task<Releaser> WriterLockAsync(CancellationToken token)
+        {
+            lock (this.waitingWriters)
+            {
+                if (this.status == 0)
+                {
+                    this.status = -1;
+                    return this.writerReleaser;
+                }
+
+                Waiter waiter = new Waiter { Tcs = new TaskCompletionSource<Releaser>() };
+                waiter.Registration = token.Register(() => this.OnWriterCancel(waiter));
+
+                this.waitingWriters.Add(waiter);
+
+                return waiter.Tcs.Task;
+            }
+        }
+
+        /// <summary>
+        /// Called when an active reader completes its work.
+        /// </summary>
+        private void ReaderRelease()
+        {
+            Waiter toWake = null;
+
+            lock (this.waitingWriters)
+            {
+                --this.status;
+                if (this.status == 0 && this.waitingWriters.Count > 0)
+                {
+                    this.status = -1;
+
+                    toWake = this.waitingWriters[0];
+                    this.waitingWriters.RemoveAt(0);
+                }
+            }
+
+            if (toWake != null)
+            {
+                toWake.Tcs.SetResult(new Releaser(this, true));
+                toWake.Registration.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Called when an active writer completes its work.
+        /// </summary>
+        private void WriterRelease()
+        {
+            Waiter writerToWake = null;
+            IEnumerable<Waiter> readersToWake = null;
+
+            lock (this.waitingWriters)
+            {
+                if (this.waitingWriters.Count > 0)
+                {
+                    writerToWake = this.waitingWriters[0];
+                    this.waitingWriters.RemoveAt(0);
+                }
+                else if (this.waitingReaders.Count > 0)
+                {
+                    this.status = this.waitingReaders.Count;
+
+                    readersToWake = this.waitingReaders.ToArray();
+                    this.waitingReaders.Clear();
+                }
+                else
+                {
+                    this.status = 0;
+                }
+            }
+
+            if (writerToWake != null)
+            {
+                writerToWake.Tcs.SetResult(new Releaser(this, true));
+                writerToWake.Registration.Dispose();
+            }
+
+            if (readersToWake != null)
+            {
+                foreach (var toWake in readersToWake)
+                {
+                    toWake.Tcs.SetResult(new Releaser(this, false));
+                    toWake.Registration.Dispose();
+                }
+            }
+        }
+
+        private void OnReaderCancel(Waiter waiter)
+        {
+            lock (this.waitingWriters)
+            {
+                if (this.waitingReaders.Count > 0)
+                {
+                    this.waitingReaders.Remove(waiter);
+                }
+            }
+
+            waiter.Tcs.TrySetCanceled();
+            waiter.Registration.Dispose();
+        }
+
+        private void OnWriterCancel(Waiter waiter)
+        {
+            IEnumerable<Waiter> readersToWake = null;
+
+            lock (this.waitingWriters)
+            {
+                if (this.waitingWriters.Count > 0)
+                {
+                    this.waitingWriters.Remove(waiter);
+                }
+
+                if (this.status > 0 && this.waitingWriters.Count == 0 && this.waitingReaders.Count > 0)
+                {
+                    this.status += this.waitingReaders.Count;
+                    readersToWake = this.waitingReaders.ToArray();
+                    this.waitingReaders.Clear();
+                }
+            }
+
+            waiter.Tcs.TrySetCanceled();
+            waiter.Registration.Dispose();
+
+            if (readersToWake != null)
+            {
+                foreach (Waiter toWake in readersToWake)
+                {
+                    toWake.Tcs.TrySetResult(new Releaser(this, false));
+                    toWake.Registration.Dispose();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Disposable Releaser to make it easy to use AsyncReaderWriterLock in a scoped manner with a using block.
+        /// </summary>
+        public struct Releaser : IDisposable
+        {
+            private readonly AsyncReaderWriterLock toRelease;
+            private readonly bool writer;
+
+            internal Releaser(AsyncReaderWriterLock toRelease, bool writer)
+            {
+                this.toRelease = toRelease;
+                this.writer = writer;
+            }
+
+            /// <inheritdoc />
+            public void Dispose()
+            {
+                if (this.toRelease != null)
+                {
+                    if (this.writer)
+                    {
+                        this.toRelease.WriterRelease();
+                    }
+                    else
+                    {
+                        this.toRelease.ReaderRelease();
+                    }
+                }
+            }
+        }
+
+        private class Waiter
+        {
+            public TaskCompletionSource<Releaser> Tcs { get; set; }
+
+            public CancellationTokenRegistration Registration { get; set; }
+        }
+    }
+}
